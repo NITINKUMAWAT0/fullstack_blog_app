48ff846b94e0ba1c6e8cc514cf1400607d486da8
Nitin Kumawat <nitin.kumawat.jaipur.in@gmail.com>
2024-11-06 17:49:24 +0530

    completed login page

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,21 +1,20 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 
 const Logo = ({ type }) => {
   return (
     <div>
-      <Link
-        to='/'
-        className={`text-2xl font-semibold ${type ? "text-white text-4xl" : "dark:text-white"}`}
+      <Link 
+        to='/' 
+        className={`text-2xl font-semibold dark:text-white ${type ? "text-white text-4xl" : ""}`}
       >
-        Blog
-        <span
-          className={`text-3xl text-green-600 ${type ? "text-5xl font-bold" : ""}`}
-        >
+        Blog 
+        <span className={`text-3xl text-green-600 ${type ? "text-5xl font-bold" : ""}`}>
           Site
         </span>
       </Link>
     </div>
   );
-};
+}
 
 export default Logo;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,18 +2,28 @@ import { useGoogleLogin } from "@react-oauth/google";
 import React, { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Toaster, toast } from "sonner";
-import Link from "react-router-dom";
-import { Logo } from "../components";
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
 
 const LoginPage = () => {
-  const user = {};
+  const user = { token: null };
 
-  if (user.token) window.location.replace("/");
-  return <div className="w-full h-[100vh">
-    <div className="hidden md:flex flex-col gap-y-4 w-1/4 min-h-screen bg-black items-center justify-between">
-    <Logo/>
+  if (user.token) {
+    window.location.replace("/");
+  }
+
+  return (
+    <div className="w-full h-[100vh]">  {/* Fixed syntax issue */}
+      <div className="hidden md:flex flex-col gap-y-4 w-1/4 min-h-screen bg-black items-center justify-between">
+        <Logo type="login"/>
+        <span className="text-xl font-semibold text-white" >Welcome, back!</span>
+      </div>
+
+      <div>
+        
+      </div>
     </div>
-    </div>;
+  );
 };
 
 export default LoginPage;
